9cdc4042d5c2e5fc5676f224f4e56dc1bd39d972
Griffin Michl <griffinmichl@gmail.com>
2016-03-13 20:15:33 -0700

    add d3 template

--- getUserData.js
+++ getUserData.js
@@ -1,22 +1,33 @@
-const Promise = require('es6-promise');
+const Promise = require('es6-promise').Promise;
 const fetch = require('isomorphic-fetch');
+const token = require('./config');
 
-function fetchLanguages(username) {
-  fetch(`https://api.github.com/users/${username}/repos`)
-    .then(response => response.json())
-    .then((repos) => {
-      console.log(repos);
-      const repoLanguages = repos.map((repo) => {
-        console.log(repo.languages_url);
-        return fetch(repo.languages_url);
-      });
-      return Promise.all(repoLanguages)
-    })
-    .then((all) => {
-      console.log('test');
-      console.log(all);
-    })
-    .catch(() => console.log('caught'))
+// potential solution to slow ajax requests
+const oboe = require('oboe');
+// const myHeaders = new Headers();
+// myHeaders.append({"Authorization": })
+
+const fetchOptions = {
+  headers: {
+    Authorization: `token ${token}`,
+  },
 }
 
-fetchLanguages('gm758');
+function getLanguageData(url) {
+  return fetch(url, fetchOptions)
+    .then(response => response.json())
+    .then(data => console.log(data))
+}
+
+function oboeLanguages(username) {
+  return oboe({
+    url: `https://api.github.com/users/${username}/repos`,
+    headers: {
+      'User-Agent': 'gm758',
+      Authorization: `token ${token}`,
+    }
+  })
+  .node('!.*.languages_url', getLanguageData)
+}
+
+oboeLanguages('gm758');
